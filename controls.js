f2c0abf983a32e1efea18243ef71d06515db1836
Shawn Beaton <beat0154@algonquinlive.com>
2020-05-17 21:57:20 -0400

    yeet

---
+++ controls.js
@@ -0,0 +1,61 @@
+var character = document.getElementById("character");
+var mousedownID = -1;
+function mousedownLeft(event) {
+  if(mousedownID==-1)mousedownID = setInterval(whilemousedownLeft, 2);
+}
+function mouseupLeft(event) {
+   if(mousedownID!=-1) {
+     clearInterval(mousedownID);
+     mousedownID=-1;
+   }
+}
+function whilemousedownLeft() {
+   var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left > 0){
+        var newleft = left - 3;
+        character.style.left = newleft + "px";
+    }
+}
+document.getElementById("left").addEventListener("mousedown", mousedownLeft);
+document.getElementById("left").addEventListener("mouseup", mouseupLeft);
+document.getElementById("left").addEventListener("touchstart", mousedownLeft);
+document.getElementById("left").addEventListener("touchend", mouseupLeft);
+
+function mousedownRight(event) {
+  if(mousedownID==-1)mousedownID = setInterval(whilemousedownRight, 2);
+}
+function mouseupRight(event) {
+   if(mousedownID!=-1) {
+     clearInterval(mousedownID);
+     mousedownID=-1;
+   }
+}
+function whilemousedownRight() {
+   var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left < 380){
+        var newleft = left + 3;
+        character.style.left = newleft + "px";
+    }
+}
+document.getElementById("right").addEventListener("mousedown", mousedownRight);
+document.getElementById("right").addEventListener("mouseup", mouseupRight);
+document.getElementById("right").addEventListener("touchstart", mousedownRight);
+document.getElementById("right").addEventListener("touchend", mouseupRight);
+
+document.addEventListener("keydown", event => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  if(event.key==="ArrowLeft"||event.key==="a"){mousedownLeft();}
+  if(event.key==="ArrowRight"||event.key==="d"){mousedownRight();}
+  
+});
+
+document.addEventListener("keyup", event => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  if(event.key==="ArrowLeft"||event.key==="a"){mouseupLeft();}
+  if(event.key==="ArrowRight"||event.key==="d"){mouseupRight();}
+  
+});
---
+++ script.js
@@ -0,0 +1,76 @@
+var game = document.getElementById("game");
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var currentBlocks = [];
+var counter = 0;
+setInterval(function(){
+    var holeLast = document.getElementById("hole".concat(counter-1));
+    var blockLast = document.getElementById("block".concat(counter-1));
+    if(counter > 0){
+        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue("top"));
+        var holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
+        if(blockLastTop < 400){
+            var block = document.createElement("div");
+            var hole = document.createElement("div");
+            block.setAttribute("class", "block");
+            block.setAttribute("id", "block".concat(counter));
+            hole.setAttribute("class", "hole");
+            hole.setAttribute("id", "hole".concat(counter));
+            block.style.top = blockLastTop + 100 + "px";
+            hole.style.top = holeLastTop + 100 + "px";
+            var random = Math.floor(Math.random() * 380);
+            hole.style.left = random + "px";
+            game.appendChild(hole); 
+            game.appendChild(block);
+            currentBlocks.push(counter);
+            counter++;
+        }
+    }
+    if(counter==0){
+        var block = document.createElement("div");
+        var hole = document.createElement("div");
+        block.setAttribute("class", "block");
+        block.setAttribute("id", "block".concat(counter));
+        hole.setAttribute("class", "hole");
+        hole.setAttribute("id", "hole".concat(counter));
+        block.style.top = blockLastTop + 100 + "px";
+        hole.style.top = holeLastTop + 100 + "px";
+        var random = Math.floor(Math.random() * 380);
+        hole.style.left = random + "px";
+        game.appendChild(hole); 
+        game.appendChild(block);
+        currentBlocks.push(counter);
+        counter++;
+    }
+},100);
+
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    let block1 = currentBlocks[0];
+    let hole1 = currentBlocks[0];
+    let block2 = document.getElementById("block".concat(block1));
+    let hole2 = document.getElementById("hole".concat(hole1));
+    var blockTop = parseInt(window.getComputedStyle(block2).getPropertyValue("top"));
+    var holeLeft = parseInt(window.getComputedStyle(hole2).getPropertyValue("left"));
+    if((holeLeft <= characterLeft && characterLeft <= holeLeft + 20) || (blockTop != 20)){
+        var newtop = blockTop - 5;
+        block2.style.top = newtop + "px";
+        hole2.style.top = newtop + "px";
+        currentBlocks.forEach(function(i){
+            let iblock = document.getElementById("block".concat(i));
+            let ihole = document.getElementById("hole".concat(i));
+            var blockTopI = parseInt(window.getComputedStyle(iblock).getPropertyValue("top"));
+            var holeLeftI = parseInt(window.getComputedStyle(ihole).getPropertyValue("left"));
+            var newtop = blockTop - 5;
+            iblock.style.top = newtop + "px";
+            ihole.style.top = newtop + "px";
+            if(blockTopI < 0){
+            console.log(currentBlocks);
+                currentBlocks.shift();
+            console.log(currentBlocks);
+            }
+        });
+    }
+}, 10);
